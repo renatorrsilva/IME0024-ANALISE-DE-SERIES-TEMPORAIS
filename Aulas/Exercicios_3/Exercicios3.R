abc74e0c61992f67a9d2eb0d2674212202e8f4f2
renato.rrsilva <renato.rrsilva@gmail.com>
2020-12-02 17:52:09 -0300

    teste

--- Aulas/Exercicios_3/Exercicios3.R
+++ Aulas/Exercicios_3/Exercicios3.R
@@ -17,42 +17,23 @@ co = ts(dat$co, start = c(1997, 1), frequency = 365)
 
 plot.ts(co)
 
-#Augmented Dickey-Fuller Test (Teste Para verificar necessidade de diferenças)
-
-adf.test(co, alternative = c("stationary"))
-
 
 #Função de Autocorrelação
 
-acf(co,lag.max=25)
+acf(co,lag.max=98)
 
 #Função de Autocorrelação Parcial
 
-pacf(co, lag.max=25)
+pacf(co, lag.max=98)
 
 
 #Modelo Preliminar
 
-mod.a = auto.arima(co,max.order = 10, lambda = "auto")
-
-checkresiduals(mod.a)
-
-
-mod=  Arima(co, order = c(25,0,0), lambda = "auto", fixed=c(NA,NA, NA, 
-                                                             0,0,0,
-                                                             0,0,0,
-                                                             0,0,0,
-                                                             NA,0,NA,
-                                                             0,0,0,
-                                                             0,0,0,
-                                                             0,0,0,
-                                                             NA,
-                                                             NA))
+mod = Arima(co, order = c(3,0,0), lambda = "auto")
 
 checkresiduals(mod)
 
-
-pacf(residuals(mod), lag.max=25)
+pacf(residuals(mod), lag.max=75)
 
 acf(residuals(mod), lag.max=25)
 
@@ -61,53 +42,13 @@ Box.test (residuals(mod), lag = 73, type = "Ljung", fitdf = 7)
 forecast(mod, h=12,biasadj=TRUE) %>% autoplot()
 
 
-##SO2
-
-
-so2 = ts(dat$so2, start = c(1997, 1), frequency = 365)
-
-#Plotando a serie
-
-plot.ts(so2)
-
-
-
-#Função de Autocorrelação
-
-acf(so2, lag.max=25)
-
-#Função de Autocorrelação Parcial
-
-pacf(so2, lag.max=25)
-
-
-#Modelo Preliminar
-
-
-mod.a = auto.arima(so2, lambda="auto", max.order = 20)
+mod.a = auto.arima(co,max.order = 30, lambda = "auto")
 
 checkresiduals(mod.a)
 
-mod=  Arima(so2, order = c(21,0,0), lambda = "auto", fixed=c(NA,NA, NA, 
-                                                            0,NA,NA,
-                                                            0,0,NA,
-                                                            NA,0,NA,
-                                                            NA,NA,NA,
-                                                            0,0,0,
-                                                            NA,NA,NA,
-                                                            NA))
+pacf(residuals(mod.a), lag.max=25)
 
-
-checkresiduals(mod)
-
-
-pacf(residuals(mod), lag.max=25)
-
-acf(residuals(mod), lag.max=25)
-
-Box.test (residuals(mod), lag = 73, type = "Ljung", fitdf = 15)
-
-forecast(mod, h=12,biasadj=TRUE) %>% autoplot()
+acf(residuals(mod.a), lag.max=25)
 
 
 
@@ -122,28 +63,21 @@ plot.ts(o3)
 
 #Função de Autocorrelação
 
-acf(o3, lag.max=25)
+acf(o3, lag.max=98)
 
 #Função de Autocorrelação Parcial
 
-pacf(o3, lag.max=25)
+pacf(o3, lag.max=98)
 
-mod.a = auto.arima(o3,max.order = 15, lambda = "auto")
+mod=  Arima(o3, order = c(1,0,0), lambda = "auto")
+
+pacf(residuals(mod), lag.max=98)
+
+acf(residuals(mod), lag.max=98)
+
+Box.test (residuals(mod), lag = 73, type = "Ljung", fitdf = 2)
+
+checkresiduals(mod)
 
 
-checkresiduals(mod.a)
-
-
-mod=  Arima(o3, order = c(13,0,0), lambda = "auto", fixed=c(NA,0,0,
-                                                             0,0,0,
-                                                             0,0,0,
-                                                             0,0,0,
-                                                             NA,
-                                                             NA))
-
-pacf(residuals(mod), lag.max=25)
-
-acf(residuals(mod), lag.max=25)
-
-Box.test (residuals(mod), lag = 73, type = "Ljung", fitdf = 3)
 
